234a40763abd6d9a629fd95c17ff7de180e5471f
lewisyau <101070737+lewisyau@users.noreply.github.com>
2023-01-22 20:25:32 +1100

    add socket.io

--- index.js
+++ index.js
@@ -3,10 +3,25 @@ var app = express()
 var cors = require("cors")
 let projectCollection;
 
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
+
+// let io = require('socket.io')(http);
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  setInterval(()=>{
+    socket.emit('number', parseInt(Math.random()*10));
+  }, 1000);
+
+});
 
 //mongoDb connection...
 const MongoClient = require('mongodb').MongoClient;
@@ -78,7 +93,7 @@ app.post('/api/projects', (req, res) => {
 })
 
 var port = process.env.port || 3000;
-app.listen(port, () => {
+http.listen(port, () => {
     console.log("App listening to http://localhost:" + port)
     createColllection("View")
 })
